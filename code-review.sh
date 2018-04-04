51dba52ce32f76a45b5f39e49ba660a089a6f7b2
Shaun C Read <shaun.c.read@gmail.com>
2018-04-04 11:26:36 +0100

    update USAGE

--- code-review.sh
+++ code-review.sh
@@ -23,7 +23,7 @@ case $1 in
 	'update' )
 		if [[ $# -ne 1 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh update"
+			echo "USAGE: code-review.sh update"
 			exit 1
 		fi
 		cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" &&
@@ -56,7 +56,7 @@ case $1 in
 	'first-time-setup' )
 		if [[ $# -ne 2 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh first-time-setup <UPSTREAM_ORGANISATION>"
+			echo "USAGE: code-review.sh first-time-setup <UPSTREAM_ORGANISATION>"
 			exit 1
 		fi
 		git config push.default simple
@@ -71,15 +71,15 @@ case $1 in
 		(git branch solutions || echo "solutions branch already exists") &&
 		git checkout master &&
 		echo "Linked to upstream repository, created solutions branch." &&
-		echo "Consider adding this script to your path in your .tcshrc/.bashrc using ./code-review.sh install for easy access when working" &&
-		echo "Now use ./code-review.sh start-task to start the latest task" &&
+		echo "Consider adding this script to your path in your .tcshrc/.bashrc using code-review.sh install for easy access when working" &&
+		echo "Now use code-review.sh start-task to start the latest task" &&
 		exit 0) ||
 		echo "Failed: have you already done first-time-setup?" && exit 0
 		;;
 	'view-solution' )
 		if [ $# -gt 3 ] || [ $# -lt 2 ]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh view-solution <USERNAME> [<BRANCH>=solutions]"
+			echo "USAGE: code-review.sh view-solution <USERNAME> [<BRANCH>=solutions]"
 			exit 1
 		elif [[ $# -eq 3 ]]; then
 			USERNAME=$2
@@ -89,7 +89,7 @@ case $1 in
 			BRANCH="solutions"
 		else
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh view-solution <USERNAME> [<BRANCH>=solutions]"
+			echo "USAGE: code-review.sh view-solution <USERNAME> [<BRANCH>=solutions]"
 			exit 1
 		fi
 		require_clean &&
@@ -108,7 +108,7 @@ case $1 in
 	'pull-tasks' )
 		if [[ $# -ne 2 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh pull-tasks"
+			echo "USAGE: code-review.sh pull-tasks"
 			exit 1
 		fi
 		require_clean &&
@@ -122,7 +122,7 @@ case $1 in
 	'start-task' )
 		if [[ $# -ne 2 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh start-task <TASK-NAME>"
+			echo "USAGE: code-review.sh start-task <TASK-NAME>"
 			exit 1
 		fi
 		require_clean &&
@@ -132,13 +132,13 @@ case $1 in
 		git merge upstream/master &&
 		git checkout -b "$2-solution" && 
 		cd "Task $2" &&
-		echo "Now on branch $2-solution, do your work in the task folder and then run ./code-review.sh finish-task to commit and upload"
+		echo "Now on branch $2-solution, do your work in the task folder and then run code-review.sh finish-task to commit and upload"
 		exit 0
 		;;
 	'finish-task' )
 		if [[ $# -ne 2 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh finish-task <TASK-NAME>"
+			echo "USAGE: code-review.sh finish-task <TASK-NAME>"
 			exit 1
 		fi
 		read -p "Submit finished task $2? [enter]"
@@ -153,7 +153,7 @@ case $1 in
 	'update-task' )
 		if [[ $# -ne 2 ]]; then
 			echo "incorrect usage"
-			echo "USAGE: ./code-review.sh update-task <TASK-NAME>"
+			echo "USAGE: code-review.sh update-task <TASK-NAME>"
 			exit 1
 		fi
 		read -p "This will pull any changes from the remote repository. Continue? [enter]"
@@ -173,7 +173,7 @@ case $1 in
 			'create-task' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop create-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop create-task <TASK-NAME>"
 					exit 1
 				fi
 				require_clean &&
@@ -184,13 +184,13 @@ case $1 in
 				git checkout -b "$3-solution" &&
 				mkdir "Task $3" && cd "Task $3"  && 
 				echo "Now make the task (including the solution) in the Task $3 folder." &&
-				echo "Commit and then use ./code-review.sh develop begin-finalise-task $3 once you're done." &&
+				echo "Commit and then use code-review.sh develop begin-finalise-task $3 once you're done." &&
 				exit 0
 				;;
 			'begin-finalise-task' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop begin-finalise-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop begin-finalise-task <TASK-NAME>"
 					exit 1
 				fi
 				require_clean &&
@@ -200,13 +200,13 @@ case $1 in
 				echo "Now:" &&
 				echo "   1. Remove your solution to the task from the task folder" &&
 				echo "   2. Commit the changes"  && 
-				echo "   3. Use ./code-review.sh develop end-finalise-task $3 to finish" &&
+				echo "   3. Use code-review.sh develop end-finalise-task $3 to finish" &&
 				exit 0
 				;;
 			'end-finalise-task' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop end-finalise-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop end-finalise-task <TASK-NAME>"
 					exit 1
 				fi
 				require_clean &&
@@ -215,14 +215,14 @@ case $1 in
 				git checkout solutions &&
 				git merge "$3-solution" -m "finish $3-solution" &&
 				echo "Task $3 has been finalised, now on solutions branch" &&
-				echo "Now use ./code-review.sh develop publish-task $3 to publish to github'" &&
-				echo "If you find that you need to change the task/solution, use ./code-review.sh develop edit-task $3" &&
+				echo "Now use code-review.sh develop publish-task $3 to publish to github'" &&
+				echo "If you find that you need to change the task/solution, use code-review.sh develop edit-task $3" &&
 				exit 0
 			        ;;
 			'publish-task' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop publish-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop publish-task <TASK-NAME>"
 					exit 1
 				fi
 				read -p "This will publish your TASK (with no solution) to github. Continue? [enter]"
@@ -230,13 +230,13 @@ case $1 in
 				cd "$TOPLEVEL" &&
 				(git push --set-upstream origin "task-$3" || exit 1) &&
 				echo "Now open a pull request against $UPSTREAM on github for task-$3" &&
-				echo "Use ./code-review.sh develop publish-solution $3 to publish the SOLUTION to this task later" &&
+				echo "Use code-review.sh develop publish-solution $3 to publish the SOLUTION to this task later" &&
 				exit 0
 				;;
 			'publish-solution' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop publish-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop publish-task <TASK-NAME>"
 					exit 1
 				fi
 				read -p "This will publish your SOLUTION ($3-solution) for task-$3 to github. Continue? [enter]"
@@ -249,7 +249,7 @@ case $1 in
 			'edit-task' )
 				if [[ $# -ne 3 ]]; then
 					echo "incorrect usage"
-					echo "USAGE: ./code-review.sh develop edit-task <TASK-NAME>"
+					echo "USAGE: code-review.sh develop edit-task <TASK-NAME>"
 					exit 1
 				fi
 				read -p "This will repoen edits on task $3 and its solution. Continue? [enter]"
@@ -257,7 +257,7 @@ case $1 in
 				cd "$TOPLEVEL" &&
 				git checkout "$3-solution" &&
 				echo "Now perform your edits to the task & solution." &&
-				echo "Run ./code-review.sh develop begin-finalise-task $3 when done" &&
+				echo "Run code-review.sh develop begin-finalise-task $3 when done" &&
 				echo "Warning: if you've published this task, people may be working on it. If you delete template files that were" &&
 				echo "previously published, they may lose their work" &&
 				exit 0
@@ -266,7 +266,7 @@ case $1 in
 esac
 
 echo "incorrect usage"
-echo "USAGE: ./code-review.sh <first-time-setup|view-solution|start-task|finish-task|update-task>"
+echo "USAGE: code-review.sh <first-time-setup|view-solution|start-task|finish-task|update-task>"
 echo "       or"
-echo "       ./code-review.sh develop <create-task|finalise-task|publish-task|publish-solution|edit-task>"
+echo "       code-review.sh develop <create-task|finalise-task|publish-task|publish-solution|edit-task>"
 exit 1
